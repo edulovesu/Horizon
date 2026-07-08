6faf3aec5e48f4b92d7c145b9fc3c0d0586f16ab
Mordi Emmanuel Chiedu <chiedumordi2330@gmail.com>
2026-07-08 10:22:07 +0100

    Added React Router module

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {browserRouter as Router} from 'react-router-dom'
 import './components/index.css'
 import App from './components/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>  
   </StrictMode>,
 )
