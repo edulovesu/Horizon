44c25c93e085ace3613660b751b1a69ee9b8bad6
Mordi Emmanuel Chiedu <chiedumordi2330@gmail.com>
2026-07-09 13:15:51 +0100

    Added extra login security

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,30 @@
+import {useState} from 'react'
 import photo from '../assets/big--image.png'
 import horizon from '../assets/Horizon-logo.png'
 
 export default function Login({ onLogin, onSignup }) {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, SetError] = useState('')
+
+  function handleLogin(){
+    if(!email || !password ){
+      SetError('Please fill in all fields')
+      return
+    }
+    if(!email.includes('@')){
+      SetError('Please enter a valid email')
+      return 
+    }
+    if(password.length < 5){
+      SetError('Password must be at lease 5 characters')
+      return 
+    }
+    SetError('')
+    onLogin()
+  }
+
   return (
     <div className='login'>
       <div className='login--container'>
@@ -13,10 +36,29 @@ export default function Login({ onLogin, onSignup }) {
         <p>Welcome back! Please enter your details</p>
         <div className='login--content'>
           <label htmlFor="Email">Email</label>
-          <input type="email" required placeholder="Enter your email" className='input1' />
+          <input 
+          type="email" required 
+          placeholder="Enter your email" 
+          className='input1'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          />
           <label htmlFor="Password">Password</label>
-          <input type="password" required placeholder="Enter your password" className='input2' />
-          <button className='login--btn' onClick={onLogin}>Login</button>
+          <input 
+          type="password" required 
+          placeholder="Enter your password" 
+          className='input2' 
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          />
+
+          {error && (
+            <p style={{color: '#ef4444', fontSize: '1.3rem', margin: '0'}}>
+              {error}
+            </p>
+          )}  
+
+          <button className='login--btn' onClick={handleLogin}>Login</button>
         </div>
         <p className='login--signup'>Don't have an account?{' '}
           <span className='blue' style={{ cursor: 'pointer' }} onClick={onSignup}> Sign up</span>
